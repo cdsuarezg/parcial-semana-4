2fb8395673d69ae25b8f3bce7c20594dd5e9a96d
Daniela Suárez <cdanisuarez@users.noreply.github.com>
2024-11-01 13:49:17 -0500

    Se agrega CoffeListComponent

--- src/app/coffee/coffee.module.ts
+++ src/app/coffee/coffee.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoffeeComponent } from './coffee.component';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CoffeeComponent]
+  declarations: [CoffeeListComponent]
 })
 export class CoffeeModule { }
