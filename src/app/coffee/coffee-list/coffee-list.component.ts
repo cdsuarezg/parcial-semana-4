169c5a90f64764dff2acb067372276f6412be3a7
Daniela Suárez <cdanisuarez@users.noreply.github.com>
2024-11-01 15:44:28 -0500

    Se calcula los tipos de cafe que llegan del servicio y se muestran

--- src/app/coffee/coffee-list/coffee-list.component.ts
+++ src/app/coffee/coffee-list/coffee-list.component.ts
@@ -9,6 +9,7 @@ import { CoffeeService } from '../coffee.service';
 })
 export class CoffeeListComponent implements OnInit {
   coffees: Array<Coffee> = [];
+  coffeeTypes: Array<{type: string; total: number}> = [];
 
   constructor(
     private coffeeService: CoffeeService,
@@ -18,11 +19,24 @@ export class CoffeeListComponent implements OnInit {
     this.getCoffees();
   }
 
+  calculateCoffeeTypes (coffees: Array<Coffee>) {
+    for (const coffee of coffees) {
+      const coffeeTypeIndex = this.coffeeTypes.findIndex(coffeeType => coffeeType.type === coffee.tipo.toLowerCase());
+
+      if (coffeeTypeIndex >= 0) {
+        this.coffeeTypes[coffeeTypeIndex].total += 1;
+      } else {
+        this.coffeeTypes.push({ type: coffee.tipo.toLowerCase(), total: 1 });
+      }
+    }
+  }
+
   getCoffees() :void {
     this.coffeeService
       .getCoffees()
       .subscribe(coffees => {
-        this.coffees = coffees
+        this.coffees = coffees;
+        this.calculateCoffeeTypes(coffees);
       });
   }
 }
